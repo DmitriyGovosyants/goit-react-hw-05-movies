21de2e31817722f60f5ac4ca9247874fede1115d
DmitriyGovosyants <Demiangvs@gmail.com>
2022-07-25 19:27:01 +0300

    fix: add strictMode

--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ import { Global, ThemeProvider } from '@emotion/react';
 import { GlobalStyles, theme } from 'styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <BrowserRouter basename="/goit-react-hw-05-movies/">
-    <ThemeProvider theme={theme}>
-      <ToastContainer />
-        <Global styles={GlobalStyles} />
-            <App />
-      </ThemeProvider>
-  </BrowserRouter>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter basename="/goit-react-hw-05-movies/">
+      <ThemeProvider theme={theme}>
+        <ToastContainer />
+          <Global styles={GlobalStyles} />
+              <App />
+        </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
